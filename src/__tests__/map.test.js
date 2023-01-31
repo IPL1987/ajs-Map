fe09ce8354b8edef4abd9e34e54e9935b2e16ee2
IPL1987 <ihorpoznyak@yandex.com>
2023-01-31 09:41:27 +0300

    test fix

--- src/__tests__/map.test.js
+++ src/__tests__/map.test.js
@@ -4,21 +4,23 @@ test('class "ErrorRepository" exists', () => {
   expect(new ErrorRepository()).toBeDefined();
 });
 
+const errorRepo = new ErrorRepository();
+
 test('class "ErrorRepository" has the "errors" property with Map object', () => {
-  expect(ErrorRepository.errors).toBeDefined();
-  expect(ErrorRepository.errors instanceof Map).toBeTruthy();
+  expect(errorRepo.errors).toBeDefined();
+  expect(errorRepo.errors instanceof Map).toBeTruthy();
 });
 
-ErrorRepository.errors.set(1, 'Описание ошибки с кодом 1');
+errorRepo.errors.set(1, 'Описание ошибки с кодом 1');
 
-test('class "ErrorRepository" throw errors', () => {
-  expect(typeof ErrorRepository).toBe('function');
+test('class "ErrorRepository" has a working "translate" method', () => {
+  expect(typeof errorRepo.translate).toBe('function');
 
-  let output = ErrorRepository(1);
+  let output = errorRepo.translate(1);
 
   expect(output).toBe('Описание ошибки с кодом 1');
 
-  output = ErrorRepository(2);
+  output = errorRepo.translate(2);
 
   expect(output).toBe('Unknown error');
 });
