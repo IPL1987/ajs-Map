e1f5349f70fc52ff035f09fb7c0520ac67c2e4eb
IPL1987 <ihorpoznyak@yandex.com>
2023-02-02 10:06:28 +0300

    add errors

--- src/__tests__/map.test.js
+++ src/__tests__/map.test.js
@@ -1,26 +1,18 @@
-import ErrorRepository from '../js/map';
+import { errorRepo } from '../js/map';
 
 test('class "ErrorRepository" exists', () => {
-  expect(new ErrorRepository()).toBeDefined();
+  expect(errorRepo).toBeDefined();
 });
-
-const errorRepo = new ErrorRepository();
 
 test('class "ErrorRepository" has the "errors" property with Map object', () => {
   expect(errorRepo.errors).toBeDefined();
   expect(errorRepo.errors instanceof Map).toBeTruthy();
 });
 
-errorRepo.errors.set(1, 'Описание ошибки с кодом 1');
+test('Known error code', () => {
+  expect(errorRepo.translate(404)).toBe('Not Found');
+});
 
-test('class "ErrorRepository" has a working "translate" method', () => {
-  expect(typeof errorRepo.translate).toBe('function');
-
-  let output = errorRepo.translate(1);
-
-  expect(output).toBe('Описание ошибки с кодом 1');
-
-  output = errorRepo.translate(2);
-
-  expect(output).toBe('Unknown error');
+test('Unknown error code', () => {
+  expect(errorRepo.translate(601)).toBe('Unknown error');
 });
--- src/js/map.js
+++ src/js/map.js
@@ -1,6 +1,13 @@
 export default class ErrorRepository {
   constructor() {
     this.errors = new Map();
+    this.errors.set(1, 'Описание ошибки с кодом 1');
+    this.errors.set(403, 'Forbidden');
+    this.errors.set(404, 'Not Found');
+    this.errors.set(408, 'Request Timeout');
+    this.errors.set(502, 'Bad Gateway');
+    this.errors.set(503, 'Service Unavailable');
+    this.errors.set(504, 'Gateway Timeout');
   }
 
   add(number, string) {
@@ -15,3 +22,5 @@ export default class ErrorRepository {
     return this.errors.get(code);
   }
 }
+
+export const errorRepo = new ErrorRepository();
